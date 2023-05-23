94e1700b36273887f263aec489e8fe5338f06c14
amin <mohammad.tasharofi0901@gmail.com>
2023-05-24 00:42:52 +0330

    third change main

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 document.querySelectorAll(".drop-zone__input").forEach(inputelement => {
     const dropZoneElement = inputelement.closest(".drop-zone");
+
+    dropZoneElement.addEventListener("dragover" , e => {
+        dropZoneElement.classList.add("drop-zone--over");
+    });
+
+    ["dragleave", "dragend"].forEach(type => {
+        dropZoneElement.addEventListener(type , e => {
+            dropZoneElement.classList.remove("drop-zone--over");
+        })
+    });
 });
